4eb33d1091ad3bf57fc30d663c45ccb111b6b5b9
LaToya Williams <platinumtoii@yahoo.com>
2018-03-20 14:32:36 -0700

    Create and complete login database query

--- src/models/db/signup.js
+++ src/models/db/signup.js
@@ -12,7 +12,8 @@ const create = (user) => {
       users (full_name, email, encrypted_password, favorite_crystal)
     VALUES ($1::text, $2::text, $3::text)
     `
-  return db.query(sql, [user.full_name, user.email, user.encrypted_password, user.favorite_crystal]).catch(error => {
+  return db.query(sql, [user.full_name, user.email, user.encrypted_password, user.favorite_crystal])
+  .catch(error => {
     console.error({
       message: 'Error occurred while executing users.create',
       arguments: arguments});
@@ -20,7 +21,8 @@ const create = (user) => {
 };
 
 const signUp = (user) => {
-  return encryptPassword(user.password).then((hash) => {
+  return encryptPassword(user.password)
+  .then((hash) => {
     user.encrypted_password = hash
     return create(user);
   }).catch(error => {
