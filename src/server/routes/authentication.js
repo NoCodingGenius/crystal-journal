a94e732502ea8ee17c4edb27236b53b12ec1525c
LaToya Williams <platinumtoii@yahoo.com>
2018-03-22 14:28:09 -0700

    Update authentication routes to include proper error handling

--- src/server/routes/authentication.js
+++ src/server/routes/authentication.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import moment from 'moment';
 import { signUp } from '../../models/db/signup';
 import { login } from '../../models/db/login';
 
 const router = express.Router()
+router.use(middleware.setDefaultResponseLocals);
 
 router.get('/signup', (req, res) => {
-  res.render('users/signup')
-})
+  res.render('users/signup', {crystal: false})
+});
 
 router.post('/signup', (req, res, next) => {
   signUp(req.body)
@@ -15,19 +17,33 @@ router.post('/signup', (req, res, next) => {
       return res.redirect('/')
   })
   .catch(next)
-})
+});
 
 router.get('/login', (req, res) => {
-  res.render('users/login')
-})
+  res.render('users/login', {
+    crystal: false,
+    error: false,
+  });
+});
 
 router.post('/login', (req, res, next) => {
   const { email, password } = req.body
   login(email, password)
   .then((user) => {
-    return res.redirect('/')
+    if (!user) {
+      const error = "Invalid username or password";
+      res.render("users/login", {
+        error: error,
+        crystal: false
+      });
+    } else {
+      req.session.user = userId;
+      res.redirect(`/profile/public/${userId}`);
+    }
   })
   .catch(next)
-})
+});
+
+router.use(middleware.isLoggedIn);
 
 export default router
